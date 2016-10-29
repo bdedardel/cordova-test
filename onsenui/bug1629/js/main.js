0f623e7629ee715934cacdb7ed64e805fc48446d
bdedardel <bdedardel@github.com>
2016-10-30 10:22:54 +1300

    [bug#1629] upgrade to onsenui 2.0.3

--- onsenui/bug1629/js/main.js
+++ onsenui/bug1629/js/main.js
@@ -2,20 +2,20 @@
 // MAIN
 // //////////////////////////////////////////////
 ons.ready(function() {
-	console.log('[onsenuilog] ' + 'ons.ready');
+	console.log('[bug1629] ' + 'ons.ready');
 
 	document.addEventListener("pageinit",
 			function(e) {
 				if (e.target.id != '') {
-					console.debug("[onsenuilog] pageinit: id='" + e.target.id
+					console.debug("[bug1629] pageinit: id='" + e.target.id
 							+ "'", e);
 				} else if (e.target.name != null && e.target.name != '') {
-					console.debug("[onsenuilog] pageinit: name='"
+					console.debug("[bug1629] pageinit: name='"
 							+ e.target.name + "'", e);
 				} else {
-					console.warn("[onsenuilog] pageinit '' => ", e.target);
+					console.warn("[bug1629] pageinit '' => ", e.target);
 				}
 			}, false);
 
-	$('#splitter-page').show();
+	//$('#splitter-page').show();
 });
